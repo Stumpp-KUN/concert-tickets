f36b58cc57eea1502c8c7cf443a9fecae2756446
Stumpp <kiril.bychkov.03@mail.ru>
2023-08-18 21:55:53 +0300

    added map and pointers

--- ConcertTicket/front/src/components/map.jsx
+++ ConcertTicket/front/src/components/map.jsx
@@ -1,21 +1,34 @@
-import React from "react";
-import {withGoogleMap, GoogleMap} from "react-google-maps";
+import React, { useEffect } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import '../css/map.css';
+import Head from './head';
 
 const Map = () => {
-    const MyGoogleMapComponent= withGoogleMap(props=>{<GoogleMap/>});
+  useEffect(() => {
+    // Инициализация карты
+    const map = L.map('map').setView([51.505, -0.09], 13);
 
-    return(
-        <div>
-            <MyGoogleMapComponent 
-            containerElement={
-                <div style={{height:`100%`}}/>
-            }
-            mapElement={
-                <div style={{height:`100%`}}/>
-            }
-            />
-        </div>
-    )
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+
+    // Создание и добавление кастомного маркера с изображением
+    const customIcon = L.icon({
+      iconUrl: 'point.png', // Укажите путь к вашему изображению
+      iconSize: [42, 42], // Размер изображения
+      iconAnchor: [16, 32], // Анкор для позиционирования
+    });
+
+    const marker = L.marker([51.5, -0.09], { icon: customIcon }).addTo(map);
+  }, []);
+
+  return (
+    <div>
+      <Head />
+      <div id="map" className="map-container"></div>
+    </div>
+  );
 }
 
 export default Map;
